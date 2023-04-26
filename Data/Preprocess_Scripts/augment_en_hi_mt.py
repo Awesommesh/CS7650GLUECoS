93765256f2945488f92393865d40df3ca9eda83f
Animesh Agrawal <animesh.a.777@gmail.com>
2023-04-25 22:57:44 -0400

    updated augmentation script to optionally use word2vec word emb substitution as well as synonym based substitution

--- Data/Preprocess_Scripts/augment_en_hi_mt.py
+++ Data/Preprocess_Scripts/augment_en_hi_mt.py
@@ -1,6 +1,7 @@
 import nlpaug.augmenter.word as naw
 import nlpaug.flow as naf
 import random
+import gensim.downloader as api
 
 def read_train_file(train_file):
     sentences = []
@@ -11,35 +12,51 @@ def read_train_file(train_file):
             sentences.append((sentence_x, sentence_y))
     return sentences
 
-def augment_sentences(sentences):
+def augment_sentences(sentences, augmentation_method):
+    model = api.load('word2vec-google-news-300')
     # define augmenter
-    augmenter = naw.SynonymAug(aug_src='wordnet', model_path=None, name='Synonym_Aug', aug_min=1, aug_max=3, aug_p=0.1, lang='eng',
+    aug_syn = naw.SynonymAug(aug_src='wordnet', model_path=None, name='Synonym_Aug', aug_min=1, aug_max=3, aug_p=0.1, lang='eng',
                      stopwords=None, tokenizer=None, reverse_tokenizer=None, stopwords_regex=None, force_reload=False,
                      verbose=0)
-
+    aug_w2v = naw.WordEmbsAug(model_type='word2vec', model=model, action="substitute", aug_min=1, aug_max=3, aug_p=0.1, top_k=10)
     # augment sentences
     augmented_sentences = []
     for sentence_x, sentence_y in sentences:
         # generate 1-2 augmented sentences
         num_aug = random.randint(1, 2)
         for _ in range(num_aug):
-            augmented_x = augmenter.augment(sentence_x)
+            augmented_x = None
+            if augmentation_method == 'synonym':
+                augmented_x = aug_syn.augment(sentence_x)
+            elif augmentation_method == "word_emb":
+                augmented_x = aug_w2v.augment(sentence)
             augmented_x = random.choice(augmented_x)
             augmented_sentence = f"{augmented_x}\t{sentence_y}"
             augmented_sentences.append(augmented_sentence)
 
     return augmented_sentences
 
-train_file = "Data/Processed_Data/MT_EN_HI/train.txt"
+if __name__ == '__main__':
+    # parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('augmentation_method', help='Augmentation method to use (synonym or word_emb)')
+    args = parser.parse_args()
+    train_file = "Data/Processed_Data/MT_EN_HI/train.txt"
 
-# read input sentences from train.txt file
-sentences = read_train_file(train_file)
+    # read input sentences from train.txt file
+    sentences = read_train_file(train_file)
+    print(len(augmented_sentences), "new sentences added")
 
-# perform synonym augmentation on input sentences
-augmented_sentences = augment_sentences(sentences)
-print(len(augmented_sentences), "new sentences added")
-# write augmented sentences to train.txt file
-with open(train_file, "a") as f:
-    f.write("\n")
-    for sentence in augmented_sentences:
-        f.write(sentence + "\n")
+    # perform appropriate augmentation on input sentences
+    all_augmented_sentences = augment_sentences(sentences, args.augmentation_method)
+
+    # write augmented sentences to train.txt file
+    with open(train_file, "a") as f:
+        f.write("\n")
+        for sentence in all_augmented_sentences:
+            f.write(sentence + "\n")
+
+
+
+
+
